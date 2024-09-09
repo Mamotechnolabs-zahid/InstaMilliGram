f318ba5e62fcb99864e1fe1e5626fd9c4bd11b4f
Yakshraj Dalwadi <yakshdalwadi1002@gmail.com>
2024-09-09 14:06:24 +0530

    monday

--- instamilligram/lib/screens/login_screen.dart
+++ instamilligram/lib/screens/login_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:instamilligram/Utilities/colors.dart';
+import 'package:instamilligram/widgets/text_field_input.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -10,6 +12,16 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose;
+    _emailController.dispose();
+    _passwordController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,17 +32,72 @@ class _LoginScreenState extends State<LoginScreen> {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
+          //to have space
+          Flexible(
+            child: Container(),
+            flex: 2,
+          ),
           //svg image
           SvgPicture.asset(
             'assets/ic_instagram.svg',
             color: primaryColor,
             height: 64,
           ),
-          const SizedBox(height: 64,),
-          //text field input 
-        
-
-
+          const SizedBox(
+            height: 64,
+          ),
+          //text field input
+          TextFieldInput(
+            textEditingController: _emailController,
+            hintText: "Enter your Email",
+            textInputType: TextInputType.emailAddress,
+          ),
+          const SizedBox(
+            height: 24,
+          ),
+          TextFieldInput(
+            textEditingController: _passwordController,
+            hintText: "Enter your password ",
+            textInputType: TextInputType.text,
+            isPass: true,
+          ),
+          const SizedBox(height: 12,),
+          InkWell(
+            child: Container(
+              child: const Text("Log in"),
+              width: double.infinity,
+              alignment: Alignment.center,
+              padding: const EdgeInsets.symmetric(vertical: 12),
+              decoration: const ShapeDecoration(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(4),
+                  ),
+                ),
+                color: blueColor,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 12,
+          ),
+          Flexible(child:Container(),flex: 2,),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                child: const Text("Don't have an account? "),
+                padding: const EdgeInsets.symmetric(vertical: 8),
+              ),
+               GestureDetector(
+                onTap: () {},
+                 child: Container(
+                  child: const Text("Sign up",style: TextStyle(fontWeight: FontWeight.bold),),
+                  padding: const EdgeInsets.symmetric(vertical: 8),
+                               ),
+               ),
+            ],
+          )
         ],
       ),
     )));
